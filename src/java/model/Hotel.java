b782e0b47e2b2e3e79dfdb292c248c7f9d17ea7c
Farhan Fath <farhanfath.a.student@gmail.com>
2024-12-18 17:16:32 +0700

    Adding Model & Controller

---
+++ src/java/model/Hotel.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author asus
+ */
+public class Hotel {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+
+    public Hotel(int id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
